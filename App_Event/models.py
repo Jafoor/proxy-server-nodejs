0b11933db5b577fa525e325483a0fcd12299dcc2
warhammer327 <rizwansm@outlook.com>
2021-06-08 01:51:26 +0600

    solved syntax problem in models.py under App_event

--- App_Event/models.py
+++ App_Event/models.py
@@ -11,21 +11,20 @@ class Profile(models.Model):
     division = models.CharField(null=False, max_length=255)
     zilla = models.CharField(null=False, max_length=255)
     upozilla = models.CharField(null=False, max_length=255)
-    village_or_ward = CharField(null=False, max_length=255)
-    home_address = CharField(null=False, max_length=255)
-    mobile_number = CharField(max_length=15, null=False)
+    village_or_ward = models.CharField(null=False, max_length=255)
+    home_address = models.CharField(null=False, max_length=255)
+    mobile_number = models.CharField(max_length=15, null=False)
     #limitation on diagram?? details required
-    banned = BooleanField(default=False)
+    banned = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
 
 class Review:
-    event_name = CharField(null=False, max_length=255)
-    user_name = CharField(null=True, max_length=255)  # for annonymous reviews
-    comment = TextField(
-        null=False, help_text="Tell us what you think about it.")
+    event_name = models.CharField(null=False, max_length=255)
+    user_name = models.CharField(null=True, max_length=255)  # for annonymous reviews
+    comment = models.TextField(null=False, help_text="Tell us what you think about it.")
     value = (
         (1, 'Worst'),
         (2, 'Bad'),
@@ -40,26 +39,26 @@ class Review:
 
 
 class Transaction:
-    donor_name = CharField(null=False, max_length=255)  # instead of username
+    donor_name = models.CharField(null=False, max_length=255)  # instead of username
     # organization that receives the payment
-    receiver_name = CharField(null=False, max_length=255)
-    amount = IntegerField(null=False)
-    date = DateTimeField(null=False)
-    hide_from_user = BooleanField(default=False)
-    for_event = BooleanField(default=False)
-    event_name = CharField(max_length=255)
+    receiver_name = models.CharField(null=False, max_length=255)
+    amount = models.IntegerField(null=False)
+    date = models.DateTimeField(null=False)
+    hide_from_user = models.BooleanField(default=False)
+    for_event = models.BooleanField(default=False)
+    event_name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 
 class AdminUses:  # this class should be renamed to ledger or something###
-    transaction_id = CharField( max_length=255)
-    amount = IntegerField()
-    total = IntegerField()
-    date = DateTimeField()
-    total_revenue = IntegerField()
-    revenue_per_transaction = IntegerField()
+    transaction_id = models.CharField( max_length=255)
+    amount = models.IntegerField()
+    total = models.IntegerField()
+    date = models.DateTimeField()
+    total_revenue = models.IntegerField()
+    revenue_per_transaction = models.IntegerField()
     #this class needs thorough discussion
 
     def __str__(self):
@@ -67,42 +66,42 @@ class AdminUses:  # this class should be renamed to ledger or something###
 
 
 class Event:
-    username_of_event_creator = CharField(null=False,max_length=255)
-    name_of_organiztion = CharField(null=False,max_length=255)
-    collection_target = IntegerField()
-    due_date = DateTimeField()
-    start_date = DateTimeField()
-    pause_donation = BooleanField(default=False)
-    is_verified = BooleanField()  # by whom ? what is the purpose of verfication ?###
-    event_detail = TextField(null=False)  # not in diagram, added for clarity
+    username_of_event_creator = models.CharField(null=False,max_length=255)
+    name_of_organiztion = models.CharField(null=False,max_length=255)
+    collection_target = models.IntegerField()
+    due_date = models.DateTimeField()
+    start_date = models.DateTimeField()
+    pause_donation = models.BooleanField(default=False)
+    is_verified = models.BooleanField()  # by whom ? what is the purpose of verfication ?###
+    event_detail = models.TextField(null=False)  # not in diagram, added for clarity
     # not necessary as link to organization's page will the available on the event page
-    organization_detail = TextField()
-    is_private = BooleanField(default=False)
-    cover_image = ImageField()
-    event_title = CharField(null=False,max_length=255)
-    received_donation_in_percentage = IntegerField()
-    total_transaction = IntegerField()  # is it the number of transaction?
+    organization_detail = models.TextField()
+    is_private = models.BooleanField(default=False)
+    cover_image = models.ImageField()
+    event_title = models.CharField(null=False,max_length=255)
+    received_donation_in_percentage = models.IntegerField()
+    total_transaction = models.IntegerField()  # is it the number of transaction?
 
     def __str__(self):
         return self.name
 
 
 class Report:
-    username = CharField(null=False,max_length=255)
-    eventID = IntegerField(null=False)
-    description = TextField()
-    date = DateTimeField()
+    username = models.CharField(null=False,max_length=255)
+    eventID = models.IntegerField(null=False)
+    description = models.TextField()
+    date = models.DateTimeField()
 
     def __str__(self):
         return self.name
 
 
 class Withdraw:
-    username = CharField(null=False,max_length=255)
-    eventID = IntegerField()
-    is_blocked = BooleanField(default=False)
-    amount = IntegerField()
-    date = DateTimeField()
+    username = models.CharField(null=False,max_length=255)
+    eventID = models.IntegerField()
+    is_blocked = models.BooleanField(default=False)
+    amount = models.IntegerField()
+    date = models.DateTimeField()
 
     def __str__(self):
         return self.name
