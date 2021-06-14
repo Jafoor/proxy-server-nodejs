42048968207603dc7159d1d3f540561571a3072b
Jafoor <abujafor.cse11@gmail.com>
2021-06-14 10:16:10 +0600

    Updated urls.py at App_Account

--- App_Account/urls.py
+++ App_Account/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('login/', views.login_view , name='login'),
     path('logout/', views.logout_view , name='logout'),
     path('email/confirmation/<str:activation_key>/', views.email_confirm, name = 'email_activation'),
-    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
-    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name = 'App_Account/password_reset_form.html'),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name = 'App_Account/password_reset_done.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = 'App_Account/password_reset_confirm.html'),name='password_reset_confirm'),
+
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name = 'App_Account/password_reset_complete.html'),name='password_reset_complete'),
     ]
