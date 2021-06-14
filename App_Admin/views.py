269c555fcf5c2f97891d53da57cf948836e87f1b
warhammer327 <rizwansm@outlook.com>
2021-06-15 05:16:50 +0600

    master page is ready for admin-panel

--- App_Admin/views.py
+++ App_Admin/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def AdminHome(request):
+    return render(request,'App_Admin/adminhome.html')
+
+def AdminHome1(request):
+    return render(request,'App_Admin/adminhome1.html')
