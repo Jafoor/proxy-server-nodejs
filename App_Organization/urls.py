d633b0c035122f5e766e50185f6f3e90e0fbf8c5
Jafoor <abujafor.cse11@gmail.com>
2021-06-15 23:33:34 +0600

    not getting value of zilla unknown cause

--- App_Organization/urls.py
+++ App_Organization/urls.py
@@ -6,7 +6,9 @@ app_name = 'App_Organization'
 urlpatterns = [
     path('organization/<slug:slug>/', views.OrgDashboard, name="OrganizationDashboard"),
     path('organization/<slug:slug>/bankinformation', views.BankInformation, name="OrganizationBankInformation"),
-    path('organization/<slug:slug>/updatebankinformation', views.UpdateBankInformation, name="OrganizationUpdateBankInformation")
+    path('organization/<slug:slug>/updatebankinformation', views.UpdateBankInformation, name="OrganizationUpdateBankInformation"),
+    path('organization/<slug:slug>/organizationinformation', views.Organizationformation, name="OrganizationInformation"),
+    path('organization/<slug:slug>/updateorganizationinformation', views.UpdateOrganizationInformation, name="UpdateOrganizationInformation"),
     # path('', views.Home, name='home'),
     # path('product/<pk>/', views.ProductDetail.as_view(), name='product_detail'),
 ]
