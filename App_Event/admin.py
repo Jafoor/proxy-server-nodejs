69952dccd261514071c14696cef633590ed8ac0d
Jafoor <abujafor.cse11@gmail.com>
2021-06-20 17:04:58 +0600

    Created Event Model

--- App_Event/admin.py
+++ App_Event/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from App_Event.models import EventType, CreateEvent
 # Register your models here.
+
+admin.site.register(EventType)
+admin.site.register(CreateEvent)
