96f7b9997ec6bdb62e77e1bc32d201e5d6a19d70
Jafoor <abujafor.cse11@gmail.com>
2021-06-22 23:49:41 +0600

    Admin Work

--- App_Admin/urls.py
+++ App_Admin/urls.py
@@ -4,11 +4,14 @@ from App_Admin import views
 app_name = 'App_Admin'
 
 urlpatterns = [
-    path('master',views.master,name='master'),
-    path('adminhome',views.AdminHome,name='adminhome'),
-    path('organizations',views.organizations,name='organizations'),
-    path('verifiedusers',views.verifiedusers,name='verifiedusers'),
-    path('generalusers',views.generalusers,name='generalusers'),
-    path('eventsbyusers',views.eventsbyusers,name='eventsbyusers'),
-    path('eventsbyorganizations',views.eventsbyorganizations,name='eventsbyorganizations'),
+    # path('master',views.master,name='master'),
+    path('dashboard/home',views.Home,name='adminhome'),
+    path('dashboard/organizationlist',views.Organizationlist,name='organizationlist'),
+    path('dashboard/eventlist', views.Eventlist,name='eventlist'),
+    path('dashboard/notverifiedeventlist', views.UnverifiedEventlist,name='notverifiedeventlist'),
+    path('dashboard/allusers', views.AllUsers,name='allusers'),
+    # path('verifiedusers',views.verifiedusers,name='verifiedusers'),
+    # path('generalusers',views.generalusers,name='generalusers'),
+    # path('eventsbyusers',views.eventsbyusers,name='eventsbyusers'),
+    # path('eventsbyorganizations',views.eventsbyorganizations,name='eventsbyorganizations'),
 ]
--- App_Admin/views.py
+++ App_Admin/views.py
@@ -1,23 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login, logout, get_user_model
+from App_Account.models import Organization
+from App_Event.models import Event
+User = get_user_model()
 
 # Create your views here.
-def master(request):
-    return render(request,'App_Admin/master.html')
+# def master(request):
+#     return render(request,'App_Admin/master.html')
 
-def AdminHome(request):
-    return render(request,'App_Admin/adminhome.html')
+def Home(request):
+    return render(request,'App_Admin/home.html')
 
-def organizations(request):
-    return render (request,'App_Admin/organizations.html')
+def Organizationlist(request):
 
-def verifiedusers(request):
-    return render (request,'App_Admin/verifiedusers.html')
+    organizations = Organization.objects.all()
 
-def generalusers(request):
-    return render (request,'App_Admin/generalusers.html')
+    context = {
+        'organizations': organizations
+    }
+    return render (request,'App_Admin/Organization/organizationlist.html', context)
 
-def eventsbyusers(request):
-    return render (request,'App_Admin/eventsbyusers.html')
+def Eventlist(request):
 
-def eventsbyorganizations(request):
-    return render (request,'App_Admin/eventsbyorganizations.html')
+    event = Event.objects.filter(active=True)
+
+    context = {
+        'event': event
+    }
+    return render (request,'App_Admin/Event/eventslist.html', context)
+
+def UnverifiedEventlist(request):
+
+    event = Event.objects.filter(active=False)
+
+    context = {
+        'event': event
+    }
+    return render (request,'App_Admin/Event/notverifiedeventslist.html', context)
+
+def AllUsers(request):
+
+    user = User.objects.all()
+
+    context = {
+        'user': user
+    }
+    return render (request,'App_Admin/User/alluser.html', context)
+
+#
+# def verifiedusers(request):
+#     return render (request,'App_Admin/verifiedusers.html')
+#
+# def generalusers(request):
+#     return render (request,'App_Admin/generalusers.html')
+#
+# def eventsbyusers(request):
+#     return render (request,'App_Admin/eventsbyusers.html')
+#
+# def eventsbyorganizations(request):
+#     return render (request,'App_Admin/eventsbyorganizations.html')
