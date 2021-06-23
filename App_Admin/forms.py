1cb78ef6a567759030849c73fde8d640e8829808
Jafoor <abujafor.cse11@gmail.com>
2021-06-23 23:57:30 +0600

    Update

---
+++ App_Admin/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from App_Account.models import Organization
+User = get_user_model()
+
+class OrganizationConfirm(forms.ModelForm):
+
+    class Meta:
+        model = Organization
+        fields = ('message', 'is_verified')
+
+    message = forms.CharField(
+        required = False,
+        widget = forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'type':'text',
+                'cols': 40,
+                'rows': 4,
+                'placeholder': 'Message for Organization',
+                'required' : False
+            }
+        )
+    )
+
+    is_verified = forms.BooleanField(required=False)
